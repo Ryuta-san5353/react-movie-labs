e22f19093caed2b50080e4cf90002c5ab89b05a1
Ryuta-san5353 <20110781@mail.wit.ie>
2024-10-16 20:47:48 +0900

    Full review page.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
-import HomePage from "./pages/homePage";
+import HomePage from "./pages/homePage"
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import {Link} from "react-router-dom";
+import MovieReviewPage from "./pages/movieReviewPage";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="/reviews/:id" element={ <MovieReviewPage/> } />
       </Routes>
     </BrowserRouter>
   );
