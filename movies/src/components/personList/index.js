0c7fc3832c629eb1f8875d8501b3852d4bf7192e
Ryuta-san5353 <20110781@mail.wit.ie>
2024-10-27 08:51:40 +0900

    created filterActorsCard

--- movies/src/components/personList/index.js
+++ movies/src/components/personList/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Actor from "../actorCard";
+import Actor from "../personCard";
 import Grid from "@mui/material/Grid2"
 
 const ActorList = (props) =>{
---
+++ movies/src/components/templateActorListPage/index.js
@@ -0,0 +1,2 @@
+import React,{useState} from "react";
+import ActorList from "../personList"
