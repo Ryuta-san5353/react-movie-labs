822befd12a01bbeb03eef3b8f467d3f35c4aa75f
Ryuta-san5353 <20110781@mail.wit.ie>
2024-11-04 03:48:53 +0900

    get movie credit from tmdb api

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -147,3 +147,21 @@ export const getMovie = (args) => {
       throw error
    });
   };
+
+  export const getMovieCredits=({queryKey})=>{
+    const [,idPart]=queryKey;
+    const {id}=idPart
+    return fetch(
+      `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then((response)=>{
+      if(!response.ok){
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error)=>{
+      throw error
+    });
+  };
+
+
